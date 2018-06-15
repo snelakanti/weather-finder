d8a6e71b3d72c0b8561c18c8a39aaae9158c109e
snelakanti <vnelakanti@gmail.com>
2018-06-15 17:27:53 -0400

    add content to titles component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Titles from './components/Titles.js'
 
 class App extends React.Component {
 render(){
   return (
-  <div>Hello</div>
+  <div>
+    <Titles />
+  </div>
   );
 }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import Titles from './components/Titles.js'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
