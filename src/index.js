2ec49e9686c804acb19139f73de48232523b9b41
snelakanti <vnelakanti@gmail.com>
2018-06-17 19:20:28 -0400

    install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.js';
 import Titles from './components/Titles.js'
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
