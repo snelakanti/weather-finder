ac3f51ae9d64e46690da307f111ace1e66e2ebb9
snelakanti <vnelakanti@gmail.com>
2018-06-18 00:05:58 -0400

    final styling

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ class App extends React.Component {
     const COUNTRY = e.target.elements.country.value;
     const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${CITY},${COUNTRY}&appid=${API_KEY}`);
     const data = await API_CALL.json();
-    if(data.cod == 404)
-    {
+    if (data.cod == 404) {
       this.setState({
         temperature: undefined,
         city: undefined,
@@ -32,44 +31,56 @@ class App extends React.Component {
         description: undefined,
         error: 'Please enter valid values'
       });
-  } 
-   else if (CITY && COUNTRY){
+    }
+    else if (CITY && COUNTRY) {
       // console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ''
-    });
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    }
+    else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the values'
+      });
+    }
   }
-  else {
-    this.setState({
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: 'Please enter the values'
-    });
+  render() {
+    return (
+      <div>
+        <div className='wrapper'>
+          <div className='main'>
+            <div className='container-fluid'>
+              <div className='row'>
+                <div className='col-xs-5 title-container'>
+                  <Titles />
+                </div>
+                <div className='col-xs-7 form-container'>
+                  <Form getWeather={this.getWeather} />
+                  <Weather temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
-  }
-render(){
-  return (
-  <div>
-    <Titles />
-    <Form getWeather = {this.getWeather}/>
-    <Weather temperature={this.state.temperature}
-    city = {this.state.city}
-    country={this.state.country}
-    humidity={this.state.humidity}
-    description={this.state.description}
-    error={this.state.error}
-    />
-  </div>
-  );
-}
 };
 
 export default App; 
